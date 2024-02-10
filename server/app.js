d57d7909bbf8e6f7784591173ae55bd571c16e98
Himanshu Agarwal <himanshuaaagarwal2002@gmail.com>
2024-02-10 16:35:52 +0530

    [updated] env files and configs

--- server/app.js
+++ server/app.js
@@ -13,9 +13,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 
-app.use('/placementpilot', Routes);
+app.use('/', Routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-module.exports = app;
